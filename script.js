bce3617fabe5dca44f77006cd3b148246db029ae
hledezma03 <hledezma910@gmail.com>
2024-03-26 10:51:54 -0400

    Create event to change the color
    Use the mouseover event to change the color of the elements inside the grid when the mouse go over the item

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const userChoice = 16;
 
-const gridContainer = document.querySelector("#grid-container")
-const gridItemSize = 100 / userChoice
+const gridContainer = document.querySelector("#grid-container");
+const gridItemSize = 100 / userChoice;
 
 for (let i = 0; i < userChoice * userChoice; i++) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("item");
       gridItem.style.cssText += `width: ${gridItemSize}%;height: ${gridItemSize}`
       gridContainer.appendChild(gridItem)
-}
+};
+
+const items = document.querySelectorAll(".item");
+
+items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = 'gray';
+    });
+});
+
